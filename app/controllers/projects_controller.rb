43ac98133d3c5abb2622ac49bc0185b71ab70e54
Ivan Storck <ivanoats@gmail.com>
2013-07-11 13:26:09 -0700

    silly stuff added in class

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,15 +1,20 @@
 class ProjectsController < ApplicationController
   def index
+    logger.info "I'm in the index! yay!"
   end
 
   def new
     @project = Project.new
+    @rhinocerous = "OMG a rhinocerous!"
+    logger.info "ZOMG a new instance!"
   end
 
   def create
+    logger.info "I'm fixin to do some creatin' oh yeah"
     @project = Project.new(params[:project])
-    if @project.save
-      flash[:notice] = "Project has been created."
+    if @project.save  # true or false
+      flash[:notice] = "Project has been created RIGHT NOW."
+      logger.info "I'm about to redirect"
       redirect_to @project
     else
       flash[:alert] = "Project has not been created."
@@ -21,3 +26,11 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
   end
 end
+
+
+
+
+
+
+
+
